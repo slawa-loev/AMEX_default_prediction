670f50bd1f4a526a06311395952be441b3430c79
Yuzhe Xiao <y.xiao-1@tudelft.nl>
2022-09-01 14:33:19 +0200

    building a frame for the package

---
+++ AMEX_default_prediction/ml_logic/preprocessor.py
@@ -0,0 +1,30 @@
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer, make_column_transformer
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
+
+import numpy as np
+import pandas as pd
+
+from colorama import Fore, Style
+
+def preprocess_features(X: pd.DataFrame) -> np.ndarray:
+
+    def create_sklearn_preprocessor() -> ColumnTransformer:
+        """
+        Create a scikit-learn preprocessor
+        that transforms a cleaned dataset of shape (_, 7)
+        into a preprocessed one of different fixed shape (_, 65)
+        """
+        final_preprocessor=None
+
+        return final_preprocessor
+
+    print(Fore.BLUE + "\nPreprocess features..." + Style.RESET_ALL)
+
+    preprocessor = create_sklearn_preprocessor()
+
+    X_processed = preprocessor.fit_transform(X)
+
+    print("\n✅ X_processed, with shape", X_processed.shape)
+
+    return X_processed
--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import find_packages
 from setuptools import setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     content = f.readlines()
-requirements = [x.strip() for x in content if 'git+' not in x]
+requirements = [x.strip() for x in content if "git+" not in x]
 
 setup(name='AMEX_default_prediction',
-      version="1.0",
-      description="Project Description",
-      packages=find_packages(),
+      version="0.0.0",
+      description="AMEX default prediction model",
+      license="MIT",
+      author="AMEX default prediction team",
+      author_email="yuzhexiao93@gmail.com",
       install_requires=requirements,
-      test_suite='tests',
+      packages=find_packages(),
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/AMEX_default_prediction-run'],
       zip_safe=False)
